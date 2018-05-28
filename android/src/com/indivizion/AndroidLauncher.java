8e7dbaee4c4ff772a0547fa1e45cd6a5a8f6d118
haroun.smida <smida.haroun@gmail.com>
2018-05-28 11:30:40 +0100

    Create Project

---
+++ android/src/com/indivizion/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.indivizion;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.indivizion.TRexGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TRexGame(), config);
+	}
+}
